dce838dacfb7aa615ab554a1048ccd797c1b8071
maru <maruoka@audio-technica.co.jp>
2023-08-01 12:49:05 +0900

    local

---
+++ 91_git pull.bat
@@ -0,0 +1,8 @@
+﻿@rem 文字コード Shift-JIS to UTF-8
+chcp 65001
+
+@echo on
+git pull
+
+@echo off
+pause
---
+++ 94_git add commit push.bat
@@ -0,0 +1,13 @@
+﻿@rem 文字コード Shift-JIS to UTF-8 変更
+chcp 65001
+
+@echo off
+set /p comment="Commit Comment："
+
+@echo on
+git add .
+git commit . -m "%comment%"
+git push -u origin main
+
+@echo off
+pause
